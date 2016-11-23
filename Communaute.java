90c56d5c9f1a1a5494e4398f353b8f0296e127c5
LiangY <lynone@ext-130-66-206-218-wifi.ec-nantes.fr>
2016-11-23 15:21:07 +0100

    classe Communaute
    écrit par Yi

--- Communaute.java
+++ Communaute.java
@@ -2,19 +2,6 @@ package medev_tp;
 
 public class Communaute extends Case{
 	// une sous-classe de Case
-	private int prix; // prix
-
-	public int getPrix() {
-		return prix;
-	}
-
-	public void setPrix(int prix) {
-		this.prix = prix;
-	}
-	public String toString(){
-		String prixString = "" + prix; // transforme prix a int
-		String retourne = getNom() + prixString;
-		return retourne;
-	}
+	
 }
 
