0a34fc98ba69381c209fdba25eb1a79af717496f
LiangY <lynone@ext-130-66-206-218-wifi.ec-nantes.fr>
2016-11-23 15:21:31 +0100

    classe Prison
    écrit par Yi

--- Prison.java
+++ Prison.java
@@ -1,1 +1,20 @@
+package medev_tp;
+
+public class Prison extends Case{
+	// une sous-classe de Case
+	private int prix; // prix
+
+	public int getPrix() {
+		return prix;
+	}
+
+	public void setPrix(int prix) {
+		this.prix = prix;
+	}
+	public String toString(){
+		String prixString = "" + prix; // transforme prix a int
+		String retourne = getNom() + prixString;
+		return retourne;
+	}
+}
 
