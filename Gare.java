6d84924888b464bccfa87b875f07ca9ddf51f0dd
ZHANGPeidong <peidong.zhang@eleves.ec-nantes.fr>
2016-11-23 16:11:12 +0100

    class gare
    modif

--- Gare.java
+++ Gare.java
@@ -1,1 +1,12 @@
+package medev_tp;
 
+// gare hérite les attributs et les méthodes par classe abstraite Achetable
+public class Gare extends Achetable{
+    @override 
+    public int calculerLoyer(){
+        int loyer;
+        loyer = 2500*this.Proprietaire().getPlateau().nbGares(this.proprietaire);
+        return loyer;
+    }
+   
+}
