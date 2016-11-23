87223f9f10dcc42feb591fc990b6214639f73c22
sdelaher <sdelaher@eleves.ec-nantes.fr>
2016-11-23 16:42:39 +0100

    Update Case.java

--- Case.java
+++ Case.java
@@ -1,22 +1,43 @@
 package medev_tp;
 
-public abstract class Case{
-	private String nom; // nom de cette case
-	private int emplacement; // emplacement de cette case
-	public String getNom() {
-		return nom;
-	}
-	public void setNom(String nom) {
-		this.nom = nom;
-	}
-	public int getEmplacement() {
-		return emplacement;
-	}
-	public void setEmplacement(int emplacement) {
-		this.emplacement = emplacement;
-	}
-	public String toString(){
-		// methode toString()
-		return this.getNom();
-	}
+public abstract class Case {
+
+    private String nom; // nom de cette case
+    private int emplacement; // emplacement de cette case
+    
+    public Case(){
+        this.nom = ""; 
+        this.emplacement = 0; 
+    }
+
+    public Case(Case c) {
+        this.nom = c.nom;
+        this.emplacement = c.emplacement;
+    }
+    
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public int getEmplacement() {
+        return emplacement;
+    }
+
+    public void setEmplacement(int emplacement) {
+        this.emplacement = emplacement;
+    }
+
+    /**
+     *
+     * @return le nom de la case
+     */
+    @Override
+    public String toString() {
+        // methode toString()
+        return this.getNom();
+    }
 }
