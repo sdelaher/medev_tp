e196e400cc94890557da462c597239dee162c21d
LiangY <lynone@ext-130-66-206-218-wifi.ec-nantes.fr>
2016-11-23 15:07:07 +0100

    Classe Case

--- Case.java
+++ Case.java
@@ -1,1 +1,19 @@
+package medev_tp;
 
+public abstract class Case{
+	private String nom; // nom de cette case
+	private int emplacement; // emplacement de cette case
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public int getEmplacement() {
+		return emplacement;
+	}
+	public void setEmplacement(int emplacement) {
+		this.emplacement = emplacement;
+	}
+	public abstract String toString(); // methode toString()
+}
