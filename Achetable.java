5d77e0f3730d86a1b1288dab383dc387e67dbd09
jiechen321 <jie.chen@eleves.ec-nantes.fr>
2016-11-23 15:36:55 +0100

    Achetable
    Jie

--- Achetable.java
+++ Achetable.java
@@ -1,1 +1,13 @@
 
+package medev_tp;
+
+public abstract class Achetable extends Case{
+    private int prix;
+    private Joueur proprietaire;
+   
+    public int calculerloyer();
+    
+    public
+
+    
+}
