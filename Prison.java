db72ab8723c745c07b0cb5be016f28128cc3e183
sdelaher <sdelaher@eleves.ec-nantes.fr>
2016-11-23 16:45:16 +0100

    Update Prison.java

--- Prison.java
+++ Prison.java
@@ -1,15 +1,28 @@
 package medev_tp;
 
-public class Prison extends Case{
-	// une sous-classe de Case
-	private int caution; // caution
+public class Prison extends Case {
+    // une sous-classe de Case
 
-	public int getCaution() {
-		return caution;
-	}
+    private int caution; // caution
 
+    public Prison() {
+        this.caution = 0;
+    }
 
-	public void setCaution(int caution) {
-		this.caution = caution;
-	}
+    public Prison(int caution) {
+        this.caution = caution;
+    }
+
+    public Prison(Prison p) {
+        super((Case) p);
+        this.caution = p.caution;
+    }
+
+    public int getCaution() {
+        return caution;
+    }
+
+    public void setCaution(int caution) {
+        this.caution = caution;
+    }
 }
